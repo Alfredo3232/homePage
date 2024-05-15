070cd404d124ff8120f504e46248719c28c0100a
Alfredo <61888719+Alfredo3232@users.noreply.github.com>
2024-05-15 12:41:15 -0700

    Comment example code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import "./style.css";
-import { setupCounter } from "./counter.js";
+// import { setupCounter } from "./counter.js";
 
-setupCounter(document.querySelector("#counter"));
+// setupCounter(document.querySelector("#counter"));
